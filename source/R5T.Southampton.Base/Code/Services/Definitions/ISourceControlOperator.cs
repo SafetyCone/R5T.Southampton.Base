1910c7dbb539726cfc775121a31c62094de5916a
MinexAutomation <minex.automation@gmail.com>
2019-12-14 14:14:06 -0800

    Added ISourceControlOperator service definition interface type.

---
+++ source/R5T.Southampton.Base/Code/Services/Definitions/ISourceControlOperator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.Southampton
+{
+    public interface ISourceControlOperator
+    {
+        void Checkout(string repositoryUrl, string localDirectoryPath);
+    }
+}
