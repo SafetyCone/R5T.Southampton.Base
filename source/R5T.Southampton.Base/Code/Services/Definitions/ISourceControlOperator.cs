998d30feb98693a133f25f38748b227e47ad883a
MinexAutomation <minex.automation@gmail.com>
2019-12-17 16:53:51 -0800

    Added Add() and CommitToRemote() methods.

--- source/R5T.Southampton.Base/Code/Services/Definitions/ISourceControlOperator.cs
+++ source/R5T.Southampton.Base/Code/Services/Definitions/ISourceControlOperator.cs
@@ -5,6 +5,8 @@ namespace R5T.Southampton
 {
     public interface ISourceControlOperator
     {
+        void Add(string localPath);
         void Checkout(string repositoryUrl, string localDirectoryPath);
+        void CommitToRemote(string localPath, string message);
     }
 }
