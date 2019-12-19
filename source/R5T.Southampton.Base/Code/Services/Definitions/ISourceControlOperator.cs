5258344fcc827846db86d4b4f39f565776a0b3ec
MinexAutomation <minex.automation@gmail.com>
2019-12-19 13:37:41 -0800

    Added operator methods.

--- source/R5T.Southampton.Base/Code/Services/Definitions/ISourceControlOperator.cs
+++ source/R5T.Southampton.Base/Code/Services/Definitions/ISourceControlOperator.cs
@@ -5,8 +5,16 @@ namespace R5T.Southampton
 {
     public interface ISourceControlOperator
     {
-        void Add(string localPath);
+        void Add(string path);
         void Checkout(string repositoryUrl, string localDirectoryPath);
-        void CommitToRemote(string localPath, string message);
+        void CommitToRemote(string path, string message);
+        /// <summary>
+        /// Given a path in a repository, return the repository root directory path.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        string GetRepositoryRootDirectoryPath(string path);
+        bool IsUnderSourceControl(string path);
+        void Update(string path);
     }
 }
