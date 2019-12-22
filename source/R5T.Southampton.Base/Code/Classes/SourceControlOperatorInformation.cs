24ea980fc567a7faf164f7bbad44bda530a05303
MinexAutomation <minex.automation@gmail.com>
2019-12-21 22:41:31 -0800

    Added SourceControlOperatorInformation class.

---
+++ source/R5T.Southampton.Base/Code/Classes/SourceControlOperatorInformation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace R5T.Southampton
+{
+    public class SourceControlOperatorInformation
+    {
+        public string Description { get; set; }
+        public Version Version { get; set; }
+    }
+}
